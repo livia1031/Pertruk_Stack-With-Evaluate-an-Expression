4a32e2ed67983e19ca711a85610b2b670c313f42
Anisa Raden-1817051007 <48753637+anisa1007@users.noreply.github.com>
2019-04-24 20:10:20 +0700

    Create Stack.cpp

---
+++ Stack.cpp
@@ -0,0 +1,7 @@
+// https://github.com/anisa1007/Pertruk_Stack-With-Evaluate-an-Expression.git
+// Atika Istiqomah 1817051004
+// Anisa Raden 1817051007
+// Livia Ayu Istoria H 1817051031
+// Mutiara Widdi A 1817051047
+
+#include <iostream>
